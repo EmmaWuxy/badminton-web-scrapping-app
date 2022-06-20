a98ff4f7b96721324196648dfb5721ec041d6b2a
EmmaWxy <emmamiao333@gmail.com>
2022-06-20 02:40:41 -0400

    Extract location, center name, time

--- __main__.py
+++ __main__.py
@@ -19,13 +19,29 @@ if response.status_code != 200:
     quit()
 
 data = response.json()
-
 # Query each center page
-r = requests.get('https://www.toronto.ca/data/parks/prd/facilities/complex/' + str(data['all'][1]['ID']) + '/index.html')
-soup = bs(r.content, 'html.parser')
-print(soup)
-#for center in data['all']:
-#    print('https://www.toronto.ca/data/parks/prd/facilities/complex/' + str(center['ID']) + '/index.html')
+for center in data['all']:
+    ctr_response = requests.get('https://www.toronto.ca/data/parks/prd/facilities/complex/' + str(center['ID']) + '/index.html')
+    ctr_soup = bs(ctr_response.content, 'html.parser')
+    try:
+        rows = ctr_soup.find('div',{'id': 'content_dropintype_Sports'}).find('table').find('tr', {'id':'dropin_Sports_0'}).find('tbody').find_all('tr')
+    except AttributeError:
+        #print('skip')
+        continue
+    #else:
+        #print('Did not skip')
+    for row in rows:
+        header = row.find('th')
+        if header is not None:
+            #print('here')
+            if header.find('span', string='Basketball', attrs={'class': 'coursetitlecol'}) is not None\
+            and header.find('span', string=' (6 - 9yrs)', attrs= {'class': 'courseagecol'}) is not None :
+                print(ctr_soup.find('h1'))
+                print(ctr_soup.find('span', attrs={'class': 'badge'}))
+                print(ctr_soup.find('span', attrs={'class': 'addressbar'}).find('strong'))
+                for time in row.find_all('td', string = lambda s: len(s)>1, attrs={'class': 'coursehrscol'}):
+                    print(time)
+                break
 
 
 #soup = bs(data['all'])
